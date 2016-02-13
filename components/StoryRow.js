442bcc8672a91fd1f893196176764a55f187cdce
Lim Chee Aun <cheeaun@gmail.com>
2016-02-13 22:43:55 +0800

    Refactor renderRow stuff into its own stateful Component
    - Easier "ref" since don't have to keep track of which row
    - Easier "state" management since its per-component

---
+++ components/StoryRow.js
@@ -0,0 +1,161 @@
+'use strict';
+
+import React, {
+  Component,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  TouchableHighlight,
+  Image,
+} from 'react-native';
+
+import LinkStore from '../stores/LinkStore';
+import LinkActions from '../actions/LinkActions';
+import domainify from '../utils/domainify';
+
+import colors from '../colors';
+
+const styles = StyleSheet.create({
+  story: {
+    backgroundColor: colors.sectionBackgroundColor,
+    flexDirection: 'row',
+    alignItems: 'stretch',
+  },
+  storyPosition: {
+    paddingTop: 10,
+    paddingLeft: 15,
+  },
+  storyPositionNumber: {
+    width: 22,
+    textAlign: 'center',
+    color: colors.insignificantColor,
+    fontSize: 17,
+  },
+  storyInfo: {
+    padding: 10,
+    flex: 1,
+  },
+  storyComments: {
+    padding: 10,
+  },
+  storyDisclosure: {
+    paddingVertical: 15,
+    paddingRight: 15,
+    paddingLeft: 5,
+  },
+  storyTitle: {
+    fontSize: 17,
+  },
+  storyTitleVisited: {
+    color: colors.insignificantColor,
+  },
+  storyDomain: {
+    fontSize: 13,
+    color: colors.domainColor,
+  },
+  storyMetadataWrap: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+  },
+  storyMetadata: {
+    fontSize: 13,
+    color: colors.insignificantColor,
+  },
+  commentIcon: {
+    width: 20,
+    height: 19,
+    marginHorizontal: 2,
+    marginTop: 3,
+    marginBottom: 2,
+  },
+  disclosureIcon: {
+    width: 8,
+    height: 13,
+    marginLeft: 2,
+    marginTop: 1,
+  },
+});
+
+export default class StoryRow extends Component {
+  constructor(props){
+    super(props);
+    const {data} = props;
+    const {links} = LinkStore.getState();
+    this.state = {
+      visited: links.includes(data.url),
+    };
+    this._onLinkChange = this._onLinkChange.bind(this);
+  }
+  componentWillMount(){
+    LinkStore.listen(this._onLinkChange);
+    LinkActions.getLinks();
+  }
+  componentWillUnmount(){
+    LinkStore.unlisten(this._onLinkChange);
+  }
+  _onLinkChange(state){
+    this.setState({
+      visited: state.links.includes(this.props.data.url),
+    });
+  }
+  _onStoryLayout(e){
+    /*
+      Temporary flexbox workaround for:
+      - https://github.com/facebook/react-native/issues/5141
+      - https://github.com/facebook/react-native/issues/1472
+    */
+    const {height} = e.nativeEvent.layout;
+    const {commentButton} = this.refs;
+    if (commentButton) commentButton.setNativeProps({ style: { height } });
+  }
+  render(){
+    const {data, position, onCommentPress, ...touchableProps} = this.props;
+    const {url, type, comments_count, points, time_ago} = data;
+    const externalLink = !/^item/i.test(url);
+    const {visited} = this.state;
+    return (
+      <TouchableHighlight {...touchableProps}>
+        <View style={styles.story}>
+          <View style={styles.storyPosition}>
+            <Text style={styles.storyPositionNumber}>{position}</Text>
+          </View>
+          <View style={styles.storyInfo} onLayout={this._onStoryLayout.bind(this)}>
+            <Text style={[styles.storyTitle, visited && styles.storyTitleVisited]}>{data.title}</Text>
+            {externalLink && <Text numberOfLines={1} style={styles.storyDomain}>{domainify(url)}</Text>}
+            {(() => {
+              if (type == 'job'){
+                return <Text style={styles.storyMetadata}>{time_ago}</Text>;
+              } else {
+                const commentsText = comments_count>0 && <Text> &middot; {comments_count} comment{comments_count != 1 && 's'}</Text>;
+                return (
+                  <View style={styles.storyMetadataWrap}>
+                    <Text style={styles.storyMetadata}>{points} point{points != 1 && 's'} by {data.user} </Text>
+                    <Text style={styles.storyMetadata}>{time_ago}{commentsText}</Text>
+                  </View>
+                );
+              }
+            })()}
+          </View>
+          {type != 'job' && (() => {
+            if (externalLink){
+              return (
+                <TouchableOpacity onPress={onCommentPress}>
+                  <View style={styles.storyComments} ref="commentButton">
+                    <Image style={styles.commentIcon} source={require('../images/comments-icon.png')}/>
+                  </View>
+                </TouchableOpacity>
+              );
+            } else {
+              return (
+                <View style={styles.storyDisclosure}>
+                  <Image style={styles.disclosureIcon} source={require('../images/disclosure-indicator.png')}/>
+                </View>
+              );
+            }
+          })()}
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}
--- views/StoriesView.js
+++ views/StoriesView.js
@@ -7,8 +7,6 @@ import React, {
   Text,
   ListView,
   TouchableOpacity,
-  TouchableHighlight,
-  Image,
   ActionSheetIOS,
 } from 'react-native';
 
@@ -16,14 +14,11 @@ import SafariView from 'react-native-safari-view';
 
 import StoryStore from '../stores/StoryStore';
 import StoryActions from '../actions/StoryActions';
-import LinkStore from '../stores/LinkStore';
-import LinkActions from '../actions/LinkActions';
 
+import StoryRow from '../components/StoryRow';
 import LoadingIndicator from '../components/LoadingIndicator';
-import HTMLView from '../components/HTMLView';
 import CommentsView from './CommentsView';
 import showBrowser from '../utils/showBrowser';
-import domainify from '../utils/domainify';
 
 import colors from '../colors';
 
@@ -55,64 +50,6 @@ const styles = StyleSheet.create({
   itemHighligtedSeparator: {
     opacity: 0,
   },
-  story: {
-    backgroundColor: colors.sectionBackgroundColor,
-    flexDirection: 'row',
-    alignItems: 'stretch',
-  },
-  storyPosition: {
-    paddingTop: 10,
-    paddingLeft: 15,
-  },
-  storyPositionNumber: {
-    width: 22,
-    textAlign: 'center',
-    color: colors.insignificantColor,
-    fontSize: 17,
-  },
-  storyInfo: {
-    padding: 10,
-    flex: 1,
-  },
-  storyComments: {
-    padding: 10,
-  },
-  storyDisclosure: {
-    paddingVertical: 15,
-    paddingRight: 15,
-    paddingLeft: 5,
-  },
-  storyTitle: {
-    fontSize: 17,
-  },
-  storyTitleVisited: {
-    color: colors.insignificantColor,
-  },
-  storyDomain: {
-    fontSize: 13,
-    color: colors.domainColor,
-  },
-  storyMetadataWrap: {
-    flexWrap: 'wrap',
-    flexDirection: 'row',
-  },
-  storyMetadata: {
-    fontSize: 13,
-    color: colors.insignificantColor,
-  },
-  commentIcon: {
-    width: 20,
-    height: 19,
-    marginHorizontal: 2,
-    marginTop: 3,
-    marginBottom: 2,
-  },
-  disclosureIcon: {
-    width: 8,
-    height: 13,
-    marginLeft: 2,
-    marginTop: 1,
-  },
   wrapper: {
     backgroundColor: colors.viewBackgroundColor,
   },
@@ -136,45 +73,30 @@ export default class StoriesView extends Component {
   constructor(props){
     super(props);
     var { stories, storiesLoading, storiesError, hasMoreStories } = StoryStore.getState();
-    var { links } = LinkStore.getState();
     this.state = {
       stories,
       loading: storiesLoading,
       error: storiesError,
       dataSource: new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2}),
-      links,
     };
     this._onChange = this._onChange.bind(this);
-    this._onLinkChange = this._onLinkChange.bind(this);
   }
   componentWillMount(){
     StoryStore.listen(this._onChange);
     StoryActions.fetchStories();
-    LinkStore.listen(this._onLinkChange);
-    LinkActions.getLinks();
   }
   componentWillUnmount(){
     StoryStore.unlisten(this._onChange);
-    LinkStore.unlisten(this._onLinkChange);
   }
   _onChange(state){
-    let stories = state.stories.map((story) => {
-      story._visited = this.state.links.includes(story.url);
-      return Object.assign({}, story);
-    });
+    const {stories, hasMoreStories} = state;
     this.setState({
       stories,
       dataSource: this.state.dataSource.cloneWithRows(stories),
       loading: state.storiesLoading,
       error: state.storiesError,
-      hasMoreStories: state.hasMoreStories,
+      hasMoreStories,
     });
-  }
-  _onLinkChange(state){
-    this.setState({
-      links: state.links,
-    });
-    this._onChange(this.state);
   }
   _navigateToComments(data){
     this.props.navigator.push({
@@ -184,72 +106,19 @@ export default class StoriesView extends Component {
       rightButtonIcon: require('../images/share-icon.png'),
       onRightButtonPress: showActivity.bind(null, `https://news.ycombinator.com/item?id=${data.id}`, data.title),
       passProps: {
-        data: data
+        data: data,
       }
     });
   }
-  _onStoryLayout(e, rowID){
-    /*
-      Temporary workaround for:
-      - https://github.com/facebook/react-native/issues/5141
-      - https://github.com/facebook/react-native/issues/1472
-    */
-    const height = e.nativeEvent.layout.height;
-    const ref = this[`_disclosure-${rowID}`];
-    if (ref) ref.setNativeProps({ style: { height } });
-  }
   _renderRow(row, sectionID, rowID, highlightRow){
-    var position = parseInt(rowID, 10) + 1;
-    var url = row.url;
-    var visited = row._visited;
-    var externalLink = !/^item/i.test(url);
-
-    var self = this;
-    var linkPress = externalLink ? showBrowser.bind(null, url) : this._navigateToComments.bind(this, row);
-    var domainText = externalLink && (<Text numberOfLines={1} style={styles.storyDomain}>{domainify(url)}</Text>);
+    const position = parseInt(rowID, 10) + 1;
+    const {url} = row;
+    const externalLink = !/^item/i.test(url);
+    const navToComments = this._navigateToComments.bind(this, row);
+    const linkPress = externalLink ? showBrowser.bind(null, url) : navToComments;
 
     return (
-      <TouchableHighlight onPress={linkPress} onLongPress={showActivity.bind(null, url, row.title)} onShowUnderlay={() => highlightRow(sectionID, rowID)} onHideUnderlay={() => highlightRow(null, null)}>
-        <View style={styles.story}>
-          <View style={styles.storyPosition}>
-            <Text style={styles.storyPositionNumber}>{position}</Text>
-          </View>
-          <View style={styles.storyInfo} onLayout={e => this._onStoryLayout.call(this, e, rowID)}>
-            <Text style={[styles.storyTitle, visited && styles.storyTitleVisited]}>{row.title}</Text>
-            {domainText}
-            {(() => {
-              if (row.type == 'job'){
-                return <Text style={styles.storyMetadata}>{row.time_ago}</Text>;
-              } else {
-                var commentsText = row.comments_count>0 && (<Text> &middot; {row.comments_count} comment{row.comments_count != 1 && 's'}</Text>);
-                return (
-                  <View style={styles.storyMetadataWrap}>
-                    <Text style={styles.storyMetadata}>{row.points} point{row.points != 1 && 's'} by {row.user} </Text>
-                    <Text style={styles.storyMetadata}>{row.time_ago}{commentsText}</Text>
-                  </View>
-                );
-              }
-            })()}
-          </View>
-          {row.type != 'job' && (() => {
-            if (externalLink){
-              return (
-                <TouchableOpacity onPress={this._navigateToComments.bind(self, row)}>
-                  <View style={styles.storyComments} ref={(component) => this[`_disclosure-${rowID}`] = component}>
-                    <Image style={styles.commentIcon} source={require('../images/comments-icon.png')}/>
-                  </View>
-                </TouchableOpacity>
-              );
-            } else {
-              return (
-                <View style={styles.storyDisclosure}>
-                  <Image style={styles.disclosureIcon} source={require('../images/disclosure-indicator.png')}/>
-                </View>
-              );
-            }
-          })()}
-        </View>
-      </TouchableHighlight>
+      <StoryRow position={position} data={row} onCommentPress={navToComments} onPress={linkPress} onLongPress={showActivity.bind(null, url, row.title)} onShowUnderlay={() => highlightRow(sectionID, rowID)} onHideUnderlay={() => highlightRow(null, null)}/>
     );
   }
   _renderSeparator(sectionID, rowID, adjacentRowHighlighted){
