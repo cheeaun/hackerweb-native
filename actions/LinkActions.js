e62b1fd495dc1cc4cac3e8c524bbaec8c468d6d8
Lim Chee Aun <cheeaun@gmail.com>
2016-04-10 11:10:12 +0800

    Remember to catch

--- actions/LinkActions.js
+++ actions/LinkActions.js
@@ -6,20 +6,24 @@ import CacheStore from 'react-native-cache-store';
 class LinkActions {
   getLinks(){
     return (dispatch) => {
-      CacheStore.get('links').then((links) => dispatch(links || []));
+      CacheStore.get('links')
+        .then((links) => dispatch(links || []))
+        .catch(() => {});
     };
   }
 
   addLink(link){
     return (dispatch) => {
-      CacheStore.get('links').then((links) => {
-        links = links || [];
-        if (!links.includes(link)){
-          dispatch(link);
-          links.unshift(link);
-          CacheStore.set('links', links.slice(0, 100));
-        }
-      });
+      CacheStore.get('links')
+        .then((links) => {
+          links = links || [];
+          if (!links.includes(link)){
+            dispatch(link);
+            links.unshift(link);
+            CacheStore.set('links', links.slice(0, 100));
+          }
+        })
+        .catch(() => {});
     };
   }
 }
