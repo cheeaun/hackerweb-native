907694495d8f75363b061d8eb94e079d1a805beb
Lim Chee Aun <cheeaun@gmail.com>
2016-06-11 00:49:29 +0800

    Upgrade to RN 0.27.x

--- ios/HackerWeb/AppDelegate.m
+++ ios/HackerWeb/AppDelegate.m
@@ -49,6 +49,7 @@
                                                       moduleName:@"HackerWeb"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
